4c767385f7268b660e24f4b12d384fe5b46e08d0
Anji265400 <bheemanianjibabu09@gmail.com>
2024-09-23 17:30:32 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -27,7 +27,6 @@ resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key = "index.html"
   source = "index.html"
-  acl = "public-read"
   content_type = "text/html"
 }
 resource "aws_s3_object" "error" {
